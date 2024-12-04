9bc48c8fd7fd3b7b2fbf88200c49bc39c6f0728c
Sebastian Labadie <sebastianlabadie.contact@gmail.com>
2024-12-04 11:58:38 -0300

    Fix HTML quotes representation in Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,7 +14,7 @@ export function Footer({ lang }: { lang: "en" | "es" }) {
   return (
     <footer className="py-[100px] bg-bgSecondary ">
       <div className="container mx-auto px-4 text-center">
-        <p className="text-4xl font-sourceSerifPro text-primary mb-10">&quot{content[lang].title}&quot</p>
+        <p className="text-4xl font-sourceSerifPro text-primary mb-10">&quot;{content[lang].title}&quot;</p>
       </div>
     </footer>
   )
